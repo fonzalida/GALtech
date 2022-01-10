e32045c00059d6e3fc13ecb32a637cd51943c14e
gfonzali <gfonzalida@lasegunda.com.ar>
2022-01-10 12:03:09 -0300

    Agregar archivos de proyecto.

---
+++ connect/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace connect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.Puerto = new System.Windows.Forms.Label();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(251, 60);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(221, 20);
+            this.txtServidor.TabIndex = 1;
+            // 
+            // Puerto
+            // 
+            this.Puerto.AutoSize = true;
+            this.Puerto.Location = new System.Drawing.Point(161, 121);
+            this.Puerto.Name = "Puerto";
+            this.Puerto.Size = new System.Drawing.Size(38, 13);
+            this.Puerto.TabIndex = 2;
+            this.Puerto.Text = "Puerto";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(251, 121);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(221, 20);
+            this.txtPuerto.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(251, 175);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(221, 20);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(251, 245);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(221, 20);
+            this.txtContraseña.TabIndex = 7;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(287, 326);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(138, 37);
+            this.btnConectar.TabIndex = 8;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.Puerto);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.Label Puerto;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
+
---
+++ connect/Form1.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace connect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+
+            string servidor = txtServidor.Text;
+            string puerto = txtPuerto.Text;
+            string usuario = txtUsuario.Text;
+            string contrasena = txtContraseña.Text;
+            string datos = "";
+
+            string cadenaConexion = "server = " + servidor + "; port = " + puerto + "; user = id= " + usuario + "; password = " + contrasena + "; database = mysql;";
+
+            MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+            try { 
+                conexionBD.Open();
+
+                MySqlDataReader reader = null;
+
+                MySqlCommand cmd = new MySqlCommand("SHOW DATABASES", conexionBD);
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    datos += reader.GetString(0) + "\n";
+                }
+            }
+            catch (MySqlException ex)
+            { 
+                MessageBox.Show(ex.ToString());
+            }
+
+            MessageBox.Show(datos);
+
+        }
+    }
+}
