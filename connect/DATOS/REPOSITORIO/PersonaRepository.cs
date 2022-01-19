63149b41d1329f6203e7c807511f5f1ec09d2818
Lean2404 <leandro.a.illanes@gmail.com>
2022-01-19 19:47:56 -0300

    Creada entidad/repository de ejemplo

---
+++ connect/DATOS/REPOSITORIO/PersonaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolSoft.DATOS.REPOSITORIO
+{
+    static class PersonaRepository
+    {
+    }
+}
