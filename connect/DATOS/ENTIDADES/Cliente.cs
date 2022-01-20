150fced77089bf9dc7aea705f11892380238e416
gfonzali <gfonzalida@lasegunda.com.ar>
2022-01-20 09:40:32 -0300

    added Client entity and repository

---
+++ connect/DATOS/ENTIDADES/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolSoft.DATOS.ENTIDADES
+{
+    public class Cliente
+    {
+       public int IdCliente { get; set; }
+       public string nombre { get; set; }
+       public string Domicilio { get; set; }
+       public int Telefono { get; set; }
+
+
+        public Cliente() { }
+    }
+}
---
+++ connect/DATOS/REPOSITORIO/ClienteRepository .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoolSoft.DATOS.ENTIDADES
+
+namespace CoolSoft.DATOS.REPOSITORIO
+{
+
+    static class ClienteRepository
+    {
+        static void agregar(Cliente p)
+        {
+            String query = "Insert into Persona (IdCLiente, nombre, domicilio, telefono) values ("p.IdCliente+","+p.nombre+","+p.Domicilio+","+p.Telefono+")";
+
+            Conexion conexion = new Conexion();
+            conexion.QueryInsert(query); 
+        }
+
+        static void eliminar(Cliente p)
+        {
+            String query = "Delete from Persona where IdCliente = "+p.IdCliente;
+
+            Conexion conexion=new Conexion();
+            conexion.QueryInsert(query);
+        }
+
+        static List<Cliente> ListarTodos(Cliente p)
+        {
+            String query = "";
+
+            Conexion conexion=new Conexion();
+            conexion.QuerySelect(query);
+        }
+    }
+}
