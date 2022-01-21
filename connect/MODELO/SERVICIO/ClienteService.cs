c483a3b79aa8779673ab65a17497e0249810bcc4
Lean2404 <leandro.a.illanes@gmail.com>
2022-01-21 14:00:01 -0300

    Adaptando arquitectura

--- connect/MODELO/SERVICIO/ClienteService.cs
+++ connect/MODELO/SERVICIO/ClienteService.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace CoolSoft.DATOS.SERVICIO
 {
-    class TecnicoService
+    static class ClienteService
     {
+        
+        
     }
 }
--- connect/MODELO/SERVICIO/TecnicoService.cs
+++ connect/MODELO/SERVICIO/TecnicoService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CoolSoft.DATOS.SERVICIO
 {
-    class ClienteService
+    static class TecnicoService
     {
     }
 }
