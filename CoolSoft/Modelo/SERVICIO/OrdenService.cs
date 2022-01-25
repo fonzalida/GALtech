ef04ada6c206200fc6650cdb37c0e2692a311eda
Lean2404 <leandro.a.illanes@gmail.com>
2022-01-25 19:46:20 -0300

    Fix OrdenRepo

--- CoolSoft/Modelo/SERVICIO/OrdenService.cs
+++ CoolSoft/Modelo/SERVICIO/OrdenService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CoolSoft.DATOS.ENTIDADES;
+using CoolSoft.DATOS.REPOSITORIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,27 @@ namespace CoolSoft.DATOS.SERVICIO
 {
     static public class OrdenService
     {
+        static public bool Agregar(Orden p, Cliente c)
+        {
+
+            if (!PersonaRepository.BuscarUno(p.dni))
+                PersonaRepository.Agregar(p);
+
+            if (TecnicoRepository.BuscarUno(p.dni))
+            {
+                MessageBox.Show("El tecnico ya se encuentra cargado");
+                return false;
+            }
+            else
+            {
+                TecnicoRepository.agregar(new Tecnico(p.dni));
+                return true;
+            }
+
+
+
+        }
+
+
     }
 }
