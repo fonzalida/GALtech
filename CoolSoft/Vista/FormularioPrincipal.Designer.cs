a463d001a0d7ab265e726e36259216785ba6ef9c
Lean2404 <leandro.a.illanes@gmail.com>
2022-01-26 21:15:40 -0300

    Modificaciones FormularioPrincipal

--- CoolSoft/Vista/FormularioPrincipal.Designer.cs
+++ CoolSoft/Vista/FormularioPrincipal.Designer.cs
@@ -30,24 +30,81 @@ namespace CoolSoft.VISTA
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormularioPrincipal));
+            this.menuStripPrincipal = new System.Windows.Forms.MenuStrip();
+            this.tecnicosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ordenesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripPrincipal.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStripPrincipal
+            // 
+            this.menuStripPrincipal.AutoSize = false;
+            this.menuStripPrincipal.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStripPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tecnicosToolStripMenuItem,
+            this.ordenesToolStripMenuItem,
+            this.clientesToolStripMenuItem});
+            this.menuStripPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuStripPrincipal.Name = "menuStripPrincipal";
+            this.menuStripPrincipal.Size = new System.Drawing.Size(150, 473);
+            this.menuStripPrincipal.TabIndex = 1;
+            this.menuStripPrincipal.Text = "menuStrip1";
+            // 
+            // tecnicosToolStripMenuItem
+            // 
+            this.tecnicosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarToolStripMenuItem});
+            this.tecnicosToolStripMenuItem.Name = "tecnicosToolStripMenuItem";
+            this.tecnicosToolStripMenuItem.Size = new System.Drawing.Size(143, 19);
+            this.tecnicosToolStripMenuItem.Text = "Tecnicos";
+            // 
+            // ordenesToolStripMenuItem
+            // 
+            this.ordenesToolStripMenuItem.Name = "ordenesToolStripMenuItem";
+            this.ordenesToolStripMenuItem.Size = new System.Drawing.Size(143, 19);
+            this.ordenesToolStripMenuItem.Text = "Ordenes";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(143, 19);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // agregarToolStripMenuItem
+            // 
+            this.agregarToolStripMenuItem.Name = "agregarToolStripMenuItem";
+            this.agregarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.agregarToolStripMenuItem.Text = "Agregar";
             // 
             // FormularioPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BackgroundImage = global::CoolSoft.Properties.Resources.LOGO_CHIQUITO;
-            this.ClientSize = new System.Drawing.Size(1163, 582);
+            this.ClientSize = new System.Drawing.Size(872, 473);
+            this.Controls.Add(this.menuStripPrincipal);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStripPrincipal;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormularioPrincipal";
             this.Text = "CoolSoft Sistema de Gestión";
             this.Load += new System.EventHandler(this.FormularioPadre_Load);
+            this.menuStripPrincipal.ResumeLayout(false);
+            this.menuStripPrincipal.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem tecnicosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ordenesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
     }
 }
