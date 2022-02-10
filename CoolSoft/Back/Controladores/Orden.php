e760c76e3e6aae589a2ebf965187fb43147ff51c
alegarcia10 <alegerman610@gmail.com>
2022-02-10 20:31:15 -0300

    actualizo datos

--- CoolSoft/Back/Controladores/Orden.php
+++ CoolSoft/Back/Controladores/Orden.php
@@ -1,15 +1,24 @@
 <?php
 
+    header('Content-Type: application/json');
     require_once("../Config/conexion.php");
     require_once("../Modelos/Morden.php");
 
     $orden = new Morden();
+
+    $body =  json_decode(file_get_contents("php://input"),true);
+
 
     switch ($_GET["op"]) {
       case 'GetAll':
         $datos=$orden->get_orden();
         echo json_encode($datos);
         break;
+
+      case 'GetDni':
+        $datos=$orden->get_orden_x_dni($body["DNICUIT"]);
+        echo json_encode($datos);
+      break;
 
       default:
         // code...
--- CoolSoft/Back/Modelos/Morden.php
+++ CoolSoft/Back/Modelos/Morden.php
@@ -1,13 +1,29 @@
 <?php
     class Morden extends Conectar{
+
       public function get_orden(){
         $conectar= parent::conexion();
         parent::set_names();
         $sql="SELECT * FROM CLIENTE";
         $sql=$conectar->prepare($sql);
         $sql->execute();
-        return $resultado=$sql->fetchAll();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
       }
+
+      public function get_orden_x_dni($dni){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT ParteOrden.Idparte, fechaInicio, FechaFin, ParteOrden.IdOrden, Completa ,Dni ,Orden.TareaDesarrollar
+        FROM ParteOrden
+      INNER JOIN TecnicoOrden ON ParteOrden.IdParte = TecnicoOrden.IdParte and TecnicoOrden.Dni = ? and ParteOrden.Completa = 1
+        INNER JOIN Orden ON ParteOrden.IdOrden = Orden.IdOrden";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1,$dni);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+      }
+
+
     }
 
  ?>
